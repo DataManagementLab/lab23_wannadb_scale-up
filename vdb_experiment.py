fc274859b32d9c2c65d3e9cb8566b35c25c4eacb
NSG22 <noelgoldschmidt@web.de>
2023-09-24 14:40:46 +0200

    added json export

--- vdb_experiment.py
+++ vdb_experiment.py
@@ -1,8 +1,7 @@
 """this experiment tests which vector indicies and are best suited for different amount of documents in a document base."""
 
-
-
 import datetime
+import json
 from wannadb.data.vector_database import compute_embedding_distances, compute_embedding_distances_withoutVDB, generate_and_store_embedding, generate_new_index, compute_new_vdb_distances
 
 import xlsxwriter
@@ -20,9 +19,9 @@ PATH = "D:\\UNI\\wannaDB\\datasets\\corona\\raw-documents"
 
 results = {}
 
-#generate_and_store_embedding(PATH, index_params)
+generate_and_store_embedding(PATH, index_params)
 
-def test_indicies():
+def test_indicies(as_json=True):
 
     for index_type in indicies_to_test:
         index_params["index_type"] = index_type
@@ -36,52 +35,58 @@ def test_indicies():
         except Exception as e:
             print(e)
             print(index_type)
-        
-    timestamp = datetime.datetime.now()
-    strftd = timestamp.strftime("%m_%d_%H_%M_%S")
-    workbook = xlsxwriter.Workbook(f"MyExcel{strftd}.xlsx")
-
-    for index_type, result in results.items():
-        times_worksheet = workbook.add_worksheet(f"{index_type}_time")
-        distances_worksheet = workbook.add_worksheet(f"{index_type}_dist")
-
-        startrow = 1
-        column = 1
-        limit_arr = []
-
-        for nprobe, probe_dict in result["vdb"].items():
-            row = startrow
             
-            times_worksheet.write(row, column, f"N_Probe = {nprobe}")
-            distances_worksheet.write(row, column, f"N_Probe = {nprobe}")
-            row +=1
-            for limit, limit_dict in probe_dict.items():
-                if not limit in limit_arr:
-                    limit_arr.append(limit)
+    if as_json:
+            
+        with open('index_tests.json', 'w') as fp:
+            json.dump(results, fp)
+    
+    else:  
+        timestamp = datetime.datetime.now()
+        strftd = timestamp.strftime("%m_%d_%H_%M_%S")
+        workbook = xlsxwriter.Workbook(f"MyExcel{strftd}.xlsx")
+
+        for index_type, result in results.items():
+            times_worksheet = workbook.add_worksheet(f"{index_type}_time")
+            distances_worksheet = workbook.add_worksheet(f"{index_type}_dist")
+
+            startrow = 1
+            column = 1
+            limit_arr = []
+
+            for nprobe, probe_dict in result["vdb"].items():
+                row = startrow
                 
-                times_worksheet.write(row, column, limit_dict["time"])
-                distances_worksheet.write(row, column, limit_dict["amount_distances"])
+                times_worksheet.write(row, column, f"N_Probe = {nprobe}")
+                distances_worksheet.write(row, column, f"N_Probe = {nprobe}")
                 row +=1
-            
+                for limit, limit_dict in probe_dict.items():
+                    if not limit in limit_arr:
+                        limit_arr.append(limit)
+                    
+                    times_worksheet.write(row, column, limit_dict["time"])
+                    distances_worksheet.write(row, column, limit_dict["amount_distances"])
+                    row +=1
+                
+                column += 1
+
             column += 1
 
-        column += 1
+            row = startrow
+            times_worksheet.write(row, column, "Without VDB")
+            distances_worksheet.write(row, column, "Without VDB")
+            row += 1
+            times_worksheet.write(row, column, result["wo_vdb"]["time"])
+            distances_worksheet.write(row, column, result["wo_vdb"]["distances"])
 
-        row = startrow
-        times_worksheet.write(row, column, "Without VDB")
-        distances_worksheet.write(row, column, "Without VDB")
-        row += 1
-        times_worksheet.write(row, column, result["wo_vdb"]["time"])
-        distances_worksheet.write(row, column, result["wo_vdb"]["distances"])
-
-        row = startrow + 1
-        column = 1    
-        for limit in limit_arr:
-            times_worksheet.write(row, column-1, f"Limit = {limit}")
-            distances_worksheet.write(row, column-1, f"Limit = {limit}")
-            row +=1
-            
-    workbook.close()
+            row = startrow + 1
+            column = 1    
+            for limit in limit_arr:
+                times_worksheet.write(row, column-1, f"Limit = {limit}")
+                distances_worksheet.write(row, column-1, f"Limit = {limit}")
+                row +=1
+                
+        workbook.close()
     
 def test_new_implementation():
     
