4dc1d677881ffa81acf2728d56e9f30699da1d45
NSG22 <noelgoldschmidt@web.de>
2023-09-25 22:41:08 +0200

    add corona loader

--- experiments/exp_2_wannadb.py
+++ experiments/exp_2_wannadb.py
@@ -23,7 +23,7 @@ from wannadb.preprocessing.other_processing import ContextSentenceCacher
 from wannadb.resources import ResourceManager
 from wannadb.statistics import Statistics
 from wannadb.status import EmptyStatusCallback
-import datasets.skyscraper.skyscraper as dataset  # TODO: choose data set by changing 'aviation' to 'corona', 'countries', 'nobel', or 'skyscrapers'
+import datasets.corona.corona as dataset  # TODO: choose data set by changing 'aviation' to 'corona', 'countries', 'nobel', or 'skyscrapers'
 from experiments.automatic_feedback import AutomaticRandomRankingBasedMatchingFeedback
 from experiments.baselines.baseline_bart_seq2seq import calculate_f1_scores
 from experiments.util import consider_overlap_as_match
