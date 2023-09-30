e35adf6cdf2ab60bf41ef479b9db356305c874b7
NSG22 <noelgoldschmidt@web.de>
2023-09-30 19:31:58 +0200

    test indicies

--- run_experiments.py
+++ run_experiments.py
@@ -1,13 +1,11 @@
 """file that starts the experiments."""
 
-from experiments.experiment2 import run_experiment_2
-from experiments.experiment_wiki import run_experiment_wiki
-from experiments.index_exp import generate_pdf, sheet_from_file, test_indicies, test_indicies_2
+#from experiments.experiment2 import run_experiment_2
+#from experiments.experiment_wiki import run_experiment_wiki
+from experiments.index_exp import test_indicies#, generate_pdf, sheet_from_file, test_indicies_2
 
 
 test_indicies(with_path=False)
-print("starting experiment 2")
-test_indicies_2(False)
 # print("finished index tests")
 # print("starting experiment 2")
 #run_experiment_wiki()
