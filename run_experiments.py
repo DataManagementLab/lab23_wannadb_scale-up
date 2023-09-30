e9a1a89475790dd43a22d9a4550dd4e38ef0329b
NSG22 <noelgoldschmidt@web.de>
2023-09-30 20:44:22 +0200

    prepare for single dist exp

--- run_experiments.py
+++ run_experiments.py
@@ -2,10 +2,12 @@
 
 #from experiments.experiment2 import run_experiment_2
 #from experiments.experiment_wiki import run_experiment_wiki
-from experiments.index_exp import test_indicies#, generate_pdf, sheet_from_file, test_indicies_2
+#from experiments.index_exp import test_indicies#, generate_pdf, sheet_from_file, test_indicies_2
+from experiments.single_distance_experiments import run_single_distance
 
 
-test_indicies(with_path=False)
+run_single_distance()
+#test_indicies(with_path=False)
 # print("finished index tests")
 # print("starting experiment 2")
 #run_experiment_wiki()
